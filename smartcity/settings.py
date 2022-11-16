952c78cfc8133d9e3d71ca8067fb27e778e11294
Rudolf Hyksa <xhyksa00@stud.fit.vutbr.cz>
2022-11-16 21:11:52 +0100

    fixed DEBUG env var checking

--- smartcity/settings.py
+++ smartcity/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = os.environ['DEBUG'] == 'True'
+# DEBUG = False
 
 ALLOWED_HOSTS = [
     'smartcity-wahoo.herokuapp.com',
