cdc8a85e06676789a37baa173e8152d82ac41d8c
Rudolf Hyksa <xhyksa00@stud.fit.vutbr.cz>
2022-11-19 20:09:41 +0100

    added ticket details view

--- smartcity_app/urls.py
+++ smartcity_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import user_views, tickets_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 userUrls = [
     path('user/login/', user_views.login),
@@ -9,11 +10,14 @@ userUrls = [
     ]
 
 ticketsUrls = [
-    path('tickets/list', tickets_views.list_tickets)
+    path('tickets/list', tickets_views.list_tickets, name='list-tickets'),
+    path('tickets/<int:id>/', tickets_views.show_ticket, name='ticket-details')
 ]
 
 testUrls = [
 ]
 
+imageUrls = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 #URLConf
-urlpatterns = userUrls + testUrls + ticketsUrls
+urlpatterns = userUrls + testUrls + ticketsUrls + imageUrls
--- smartcity_app/views/tickets_views.py
+++ smartcity_app/views/tickets_views.py
@@ -9,3 +9,13 @@ def list_tickets(request: HttpRequest) -> HttpResponse:
     tickets = Ticket.objects.all()
     return render(request, 'tickets/list.html', {'tickets': tickets})
 
+def show_ticket(request: HttpRequest, id:int) -> HttpResponse:
+    # currentUserData = getCurrentUserDict(request)
+
+    # if currentUserData == {}:
+    #     messages.warning(request, "You need to log in to visit this page.")
+    #     return HttpResponseRedirect('/user/login')
+
+
+    ticket = Ticket.objects.filter(id=id).values().first()
+    return render(request, 'tickets/details.html', ticket)
