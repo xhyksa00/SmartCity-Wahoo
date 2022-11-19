6d9ea4a9780188c9e498c8aac328ab7b2d98a8ab
Rudolf Hyksa <xhyksa00@stud.fit.vutbr.cz>
2022-11-19 20:06:51 +0100

    added ticket listing

--- smartcity_app/views/tickets_views.py
+++ smartcity_app/views/tickets_views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from ..models import Ticket, User
+from .helpers import getCurrentUserDict
+from django.contrib import messages
+from ..forms import ticket_forms
 
 def list_tickets(request: HttpRequest) -> HttpResponse:
-    data = Ticket.objects.all()
-    return render(request, 'tickets/list.html', {'tickets': data})
+    tickets = Ticket.objects.all()
+    return render(request, 'tickets/list.html', {'tickets': tickets})
+
