c0e50902af86fd15e05e27072c061e571113f4c1
Rudolf Hyksa <xhyksa00@stud.fit.vutbr.cz>
2022-11-28 23:17:28 +0100

    static moved

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-zip -r xnemce07.zip doc.html requiremetns.txt manage.py smartcity static
+zip -r xnemce07.zip doc.html requiremetns.txt manage.py smartcity smartcity_admin smartcity_app static database_schema.png
 # add other files obvs
