4f11885a3974de3324f532636e9a7b303e732e9b
Rudolf Hyksa <xhyksa00@stud.fit.vutbr.cz>
2022-11-29 01:14:58 +0100

    deleted dt and admin css and js (not used)

--- smartcity/settings.py
+++ smartcity/settings.py
@@ -143,8 +143,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/css"),
-    os.path.join(BASE_DIR, "static/js")
+    os.path.join(BASE_DIR, "static/"),
+    # os.path.join(BASE_DIR, "static/js")
 ]
 
 MEDIA_URL = '/media/'
