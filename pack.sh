170da36e7480b8aa1609e950be52ed498121458b
Rudolf Hyksa <xhyksa00@stud.fit.vutbr.cz>
2022-12-02 02:21:56 +0100

    last fix

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-zip -r xnemce07.zip doc.html requiremetns.txt manage.py smartcity smartcity_admin smartcity_app database_schema.png runtime.txt Procfile SmartCity.sql
+zip -r xnemce07.zip doc.html requirements.txt manage.py smartcity smartcity_admin smartcity_app database_schema.png runtime.txt Procfile SmartCity.sql
 # add other files obvs
